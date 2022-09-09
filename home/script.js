db6304806f44327607e82c601b777200e25f1608
yufan chen <s20315103@stu.edu.tw>
2022-09-09 18:34:37 +0800

    v8

--- home/script.js
+++ home/script.js
@@ -1,4 +1,5 @@
 var door = document.getElementById("addr");
+var waterControl = document.getElementById("water");
 door.addEventListener("click",function(){
     var password = 123456;
     var input;
@@ -20,14 +21,34 @@ door.addEventListener("click",function(){
     }
     else{
         alert("登入成功")
-        // document.getElementById("test").innerHTML= '<a href="http://120.119.67.10:5122">鐵捲門</a>'
-        // document.getElementById("addr").innerHTML = "鐵捲門"
-        // .scc = '<a href="http://120.119.67.10:55122">鐵捲門</a>'
         this.href = "http://120.119.67.10:55122"
     }
-    // var door = document.getElementById("addr");
-    // door.addEventListener("click",function(){
 })
+waterControl.addEventListener("click",function(){
+    var password = 1234;
+    var input;
+    var entry_count = 0;
+    var entry_limit = 3;
+    var out_of_limit = false;
+
+    while(password!=input && !out_of_limit){
+        entry_count++;
+        if(entry_count<=entry_limit){
+            input = prompt("請輸入密碼")
+        }
+        else{
+            out_of_limit = true;
+        }
+    }
+    if(out_of_limit){
+        alert("輸入錯誤超過次數")
+    }
+    else{
+        alert("登入成功")
+        this.href = "http://120.119.67.10:55200"
+    }
+})
+
     // function change() {
     //     document.body.style.background = document.getElementById("colors").value
     // }

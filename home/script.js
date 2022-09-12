5895a1e57e4f08f9cf30de1f57f292c96a8a0e49
yufan chen <s20315103@stu.edu.tw>
2022-09-12 14:59:49 +0800

    v33

--- home/script.js
+++ home/script.js
@@ -48,6 +48,30 @@ waterControl.addEventListener("click",function(){
         this.href = "http://120.119.67.10:55168"
     }
 })
+waterControl.addEventListener("click",function(){
+    var password = 0618;
+    var input;
+    var entry_count = 0;
+    var entry_limit = 3;
+    var out_of_limit = false;
+
+    while(password!=input && !out_of_limit){
+        entry_count++;
+        if(entry_count<=entry_limit){
+            input = prompt("請輸入密碼")
+        }
+        else{
+            out_of_limit = true;
+        }
+    }
+    if(out_of_limit){
+        alert("輸入錯誤超過次數")
+    }
+    else{
+        alert("登入成功")
+        this.href = "http://120.119.67.10:55136"
+    }
+})
     // function change() {
     //     document.body.style.background = document.getElementById("colors").value
     // }

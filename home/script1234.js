92ffe5c0398f18468e93679b3d4378989fad9dd8
yufan chen <s20315103@stu.edu.tw>
2022-09-16 14:43:41 +0800

    v35

--- home/script1234.js
+++ home/script1234.js
@@ -1,6 +1,11 @@
 var door = document.getElementById("addr");
-var waterControl = document.getElementById("water");
-var wush = document.getElementById("wus");
+// var waterControl = document.getElementById("water");
+var wush = document.getElementById("wushi");
+var water = document.getElementById("wat")
+
+document.getElementById("colors").addEventListener("change", function() {
+    document.body.style.background = document.getElementById("colors").value
+})
 door.addEventListener("click",function(){
     var password = 123456;
     var input;
@@ -25,32 +30,8 @@ door.addEventListener("click",function(){
         this.href = "http://120.119.67.10:55122"
     }
 })
-// waterControl.addEventListener("click",function(){
-//     var password = 1234;
-//     var input;
-//     var entry_count = 0;
-//     var entry_limit = 3;
-//     var out_of_limit = false;
-
-//     while(password!=input && !out_of_limit){
-//         entry_count++;
-//         if(entry_count<=entry_limit){
-//             input = prompt("請輸入密碼")
-//         }
-//         else{
-//             out_of_limit = true;
-//         }
-//     }
-//     if(out_of_limit){
-//         alert("輸入錯誤超過次數")
-//     }
-//     else{
-//         alert("登入成功")
-//         this.href = "http://120.119.67.10:55168"
-//     }
-// })
-wush.addEventListener("click",function(){
-    var password = 12345;
+water.addEventListener("click",function(){
+    var password = 1234;
     var input;
     var entry_count = 0;
     var entry_limit = 3;
@@ -70,12 +51,36 @@ wush.addEventListener("click",function(){
     }
     else{
         alert("登入成功")
-        this.href = "http://120.119.67.10:55122"
+        this.href = "http://120.119.67.10:55168"
     }
 })
-    // function change() {
-    //     document.body.style.background = document.getElementById("colors").value
-    // }
-    document.getElementById("colors").addEventListener("change", function() {
-        document.body.style.background = document.getElementById("colors").value
+wush.addEventListener("click",function(){
+    var password = 0618;
+    var input;
+    var entry_count = 0;
+    var entry_limit = 3;
+    var out_of_limit = false;
+
+    while(password!=input && !out_of_limit){
+        entry_count++;
+        if(entry_count<=entry_limit){
+            input = prompt("請輸入密碼")
+        }
+        else{
+            out_of_limit = true;
+        }
+    }
+    if(out_of_limit){
+        alert("輸入錯誤超過次數")
+    }
+    else{
+        alert("登入成功")
+        this.href = "http://120.119.67.10:55136"
+    }
 })
+//    function change() {
+//         document.body.style.background = document.getElementById("colors").value
+//     }
+    // document.getElementById("colors").addEventListener("change", function() {
+    //     document.body.style.background = document.getElementById("colors").value
+    // })
